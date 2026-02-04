f1469a771fd2bb441c9694153cb2cfb3621508e1
hongkim25 <for20170101@gmail.com>
2026-02-04 14:04:19 +0900

    nav bar padding change

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Navbar() {
   return (
     <nav className="w-full flex justify-between items-center bg-[var(--nav-bg)] text-[var(--nav-text)] transition-colors duration-500 ease-in-out leading-none px-4 py-3
-  md:px-8 md:py-4">
+  md:px-4 md:py-4">
       {/* Logo */}
       <Link href="/" className="text-[5vw] md:text-[3.5vw] font-bold hover:text-black transition-colors leading-none">
         HONG KIM
